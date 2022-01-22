682522ef3096ca3ac96d4d26aa51193231ae090d
yoav z <mail.yoav@gmail.com>
2022-01-22 20:13:42 +0200

    update to prefereed android xml arguments and some tidyness

--- app/src/main/java/xyz/yoav/materialclock/ClockWidgetProvider.java
+++ app/src/main/java/xyz/yoav/materialclock/ClockWidgetProvider.java
@@ -1,21 +1,13 @@
 package xyz.yoav.materialclock;
 
-import android.app.PendingIntent;
+//import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.icu.text.SimpleDateFormat;
-import android.net.Uri;
-import android.util.Log;
 import android.widget.RemoteViews;
-import android.widget.Toast;
-
-import java.util.Date;
-import java.util.Random;
-
 import static android.content.Context.MODE_PRIVATE;
 
 public class ClockWidgetProvider extends AppWidgetProvider implements WidgetUpdatedInterface {
@@ -29,7 +21,6 @@ public class ClockWidgetProvider extends AppWidgetProvider implements WidgetUpda
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
-        //Log.d("yoav", "## onReceive " + intent.getAction());
     }
 
     private void redrawWidgetFromData(Context context, AppWidgetManager appWidgetManager, int widgetId) {
